440cd05939afca3e336977451b94a1296f920045
dimanvaz <dimanvaz04@gmail.com>
2021-11-10 00:34:58 +0300

    fix path to imgs?

--- Routes/Home.js
+++ Routes/Home.js
@@ -1,10 +1,10 @@
 export const Home = {
     template: `<div class="main-container">
-            <img src="../pics/main-mobs-cut.jpg" alt="" class="for-mobs">
-                <img src="../pics/main-Man-tabs-1024x768.jpg" alt="" class="for-tabs">
-                <img src="../pics/main-woMan-tabs-1024x768.jpg" alt="" class="for-tabs">
-                    <img src="../pics/main-Man-PC-900x900.jpg" alt="" class="for-PC">
-                    <img src="../pics/main-woMan-PC-900x900.jpg" alt="" class="for-PC">
+            <img src="./pics/main-mobs-cut.jpg" alt="" class="for-mobs">
+                <img src="./pics/main-Man-tabs-1024x768.jpg" alt="" class="for-tabs">
+                <img src="./pics/main-woMan-tabs-1024x768.jpg" alt="" class="for-tabs">
+                    <img src="./pics/main-Man-PC-900x900.jpg" alt="" class="for-PC">
+                    <img src="./pics/main-woMan-PC-900x900.jpg" alt="" class="for-PC">
 
         </div>`
 }
