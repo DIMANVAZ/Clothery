26f27b87e36c676398983902f39063ca2eef1a94
dimanvaz <dimanvaz04@gmail.com>
2021-11-28 02:03:08 +0300

    need to refractor Cart

--- Routes/CartRoute.js
+++ Routes/CartRoute.js
@@ -6,17 +6,25 @@ export const CartRoute = {
                 <table v-if="this.cart.totalItems() > 0">
                     <caption>Товары в вашей корзине</caption>
                     <tr>
-                        <th>Позиция</th>
-                        <th>Размер</th>
-                        <th>Штук</th>
-                        <th>На сумму</th>
+                        <th class="table position">Позиция</th>
+                        <th class="table size_pcs">Размер : штук</th>
+                        <th class="table price_per_1">Цена за шт</th>
+                        <th class="table price_per_position">Сумма по позиции</th>
                     </tr>
-                    <tr v-for="item in this.cart.cartBox">
-                        <td>{{item.name}} </td>
-                        <!--<img :src="item?.media?.images[0].image160pxUrl" alt="">-->
-                        <td>Размер</td>
-                        <td>{{item.count}}</td>
-                        <td>{{item.price}}</td>
+                    <tr v-for="elem in this.cart.cartBox">
+                        <td class="table position">
+                            {{elem.position.name.slice(9)}} <br>
+                            <img :src="elem.position?.media?.images[0].image160pxUrl" alt="">
+                        </td>
+                        <td class="table size_pcs">
+                            <div v-for="size in Object.keys(elem.ordered)">{{size}} : {{elem.ordered[size]}}</div>
+                        </td>
+                        <td class="table price_per_1">
+                            {{elem.position.price}} ₽
+                        </td>
+                        <td class="table price_per_position">
+                            {{this.sizesSum(elem) * elem.position.price}} ₽
+                        </td>
                     </tr>
                 </table>
                 <button v-if="this.cart.totalItems() > 0" id="orderRequestButton" @click="this.showDialog()">Оформить заказ</button>
@@ -27,6 +35,11 @@ export const CartRoute = {
     methods:{
         showDialog(){
             document.querySelector('dialog').setAttribute('open','')
+        },
+        sizesSum(elem){
+            return Object.values(elem.ordered).reduce((start, item) => {
+                return start + item
+            })
         }
     }
 }
