8f8512d907cc232b79d175c30c68fc905673921c
dimanvaz <dimanvaz04@gmail.com>
2021-11-30 16:29:44 +0300

    feat: router to item from cart

--- Routes/CartRoute.js
+++ Routes/CartRoute.js
@@ -6,6 +6,22 @@ export const CartRoute = {
                     <span v-if="this.cart.totalItems() > 0">: всего товаров {{this.cart.totalItems()}} на сумму {{this.cart.totalPrice()}} руб</span>
                     <span v-if="this.cart.totalItems() === 0">пуста... </span>
                 </h2>
+                <div v-if="this.cart.totalItems() > 0">
+                     <tr v-for="(position,i) in this.cart.cartBox">
+                                <router-link :to="'/item/' + position.item.id">
+                            <td > {{position?.item.name.slice(9)}} <br>
+                                <img :src="position.item?.media?.images[0].image160pxUrl" alt="160">
+                            </td>
+                                </router-link>
+                            <td class="table size">{{ Object.keys(position)[1] }}</td>
+                            <td class="table pcs">{{ Object.values(position)[1] }}</td>
+                            <td class="table price_per_position">{{ Object.values(position)[1] * position.item.price }}</td>
+                            <td class="table delete-item-x" :data="i" @click="this.cart.removeOneLine(i),this.cart.saveToLS()"> X </td>
+                    </tr>
+                </div>
+                
+                
+                
                 <table v-if="this.cart.totalItems() > 0">
                     <caption></caption>
                     <tr>
@@ -16,9 +32,11 @@ export const CartRoute = {
                         <th class="table delete-item">Удалить</th>
                     </tr>
                     <tr v-for="(position,i) in this.cart.cartBox">
+                                <router-link :to="'/item/' + position.item.id">
                             <td > {{position?.item.name.slice(9)}} <br>
                                 <img :src="position.item?.media?.images[0].image160pxUrl" alt="160">
                             </td>
+                                </router-link>
                             <td class="table size">{{ Object.keys(position)[1] }}</td>
                             <td class="table pcs">{{ Object.values(position)[1] }}</td>
                             <td class="table price_per_position">{{ Object.values(position)[1] * position.item.price }}</td>
