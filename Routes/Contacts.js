9d7a73e09b349b06b608b331e9dc0e7fa392e1b0
dimanvaz <dimanvaz04@gmail.com>
2021-12-05 00:50:04 +0300

    need to make anchor in Collections + refractor About

--- Routes/Contacts.js
+++ Routes/Contacts.js
@@ -1,10 +1,11 @@
 export const Contacts = {
-    template:`<div class="Contacts contacts-component">
-
-              Официальный представитель Clothery, Inc. в России: <br>
+    template:`<div class="Contacts-whole-container">
+                
+              <p  class="Contacts-description">Официальный представитель Clothery, Inc. в России: <br>
               "ООО Клоузери Раша", ОГРН 3899 9898 1239 1239, <br>
               Юридический и фактический адрес: г.Москва, Хрущёвский Вал, 11Б, БЦ Лакшери Голд Плаза, офис 211<br>
-              <div id="key_figures">
+              </p>
+              <div class="Contacts-figures">
                 <figure> 
                     <img src="./pics/Gen_dir.jpg" alt="CEO portrait" style="max-width:100%">
                     <figcaption>Генеральный директор Фишмансон Савва Ааронович</figcaption>
@@ -15,10 +16,12 @@ export const Contacts = {
               </figure>
               </div>    
  
+              <p class="Contacts-description">
               Отдел продаж и обслуживания физических лиц: <br>
               +7-495-190-48-33, email shop@clothery.com<br>
               Отдел по работе с юридическими лицами:<br>
               +7-495-190-48-93, email b2b@clothery.com<br>
+              </p>
              
               </div>`
 }
