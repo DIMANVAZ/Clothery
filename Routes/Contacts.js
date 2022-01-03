6fcc379bf20607dbf30d4895e662daca074f4307
dimanvaz <dimanvaz04@gmail.com>
2022-01-03 16:19:48 +0300

    adapted Contacts for 1201

--- Routes/Contacts.js
+++ Routes/Contacts.js
@@ -1,28 +1,38 @@
 export const Contacts = {
     template:`<div class="Contacts-whole-container">
                 
-              <p  class="Contacts-description">Официальный представитель Clothery, Inc. в России: <br>
-              "ООО Клоузери Раша", ОГРН 3899 9898 1239 1239, <br>
-              Юридический и фактический адрес: г.Москва, Хрущёвский Вал, 11Б, БЦ Лакшери Голд Плаза, офис 211<br>
-              </p>
+
               
               <div class="Contacts-figures">
                 <figure> 
-                    <img src="./pics/Gen_dir.jpg" alt="CEO portrait">
-                    <figcaption>Генеральный директор <br> Фишмансон Савва Ааронович</figcaption>
+                    <img src="./pics/Gen_dir.jpg" class="contacts-pic" alt="CEO portrait">
+                    <figcaption>
+                    Генеральный директор <br> 
+                    Фишмансон Савва Ааронович <br> 
+                    email: ceo_rus@clothery.com
+                    </figcaption>
                 </figure> 
                 <figure> 
-                    <img src="./pics/Customer_service.jpg" alt="Customer service portrait">
-                    <figcaption>Менеджер по работе с клиентами <br> Либкина Берта Моисеевна</figcaption>
+                    <img src="./pics/Customer_service.jpg" class="contacts-pic" alt="Customer service portrait">
+                    <figcaption>
+                    Менеджер по работе с клиентами <br> 
+                    Либкина Берта Моисеевна <br>
+                    email: customer_rus@clothery.com
+                    </figcaption>
               </figure>
+
+              <div class="Contacts-description">
+                  <p>Официальный представитель Clothery, Inc. в России: <br>
+                  "ООО Клоузери Раша", ОГРН 3899 9898 1239 1239, <br>
+                  Юридический и фактический адрес: <br>г.Москва, Хрущёвский Вал, 11Б, БЦ Лакшери Голд Плаза, офис 211<br>
+                  </p>
+                  <p>Отдел продаж и обслуживания физических лиц: <br>
+                  +7-495-190-48-33, email: shop@clothery.com<br></p>
+                  <p>Отдел по работе с юридическими лицами:<br>
+                  +7-495-190-48-93, email: b2b@clothery.com<br></p>
+              </div>
+              
               </div>    
- 
-              <p class="Contacts-description">
-                  Отдел продаж и обслуживания физических лиц: <br>
-                  +7-495-190-48-33, email shop@clothery.com<br>
-                  Отдел по работе с юридическими лицами:<br>
-                  +7-495-190-48-93, email b2b@clothery.com<br>
-              </p>
-             
-              </div>`
+
+         </div>`
 }
