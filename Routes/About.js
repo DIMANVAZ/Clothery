8b47dabf63401e9928c3ef519f3b6fb10ce7bedc
dimanvaz <dimanvaz04@gmail.com>
2022-01-04 19:23:05 +0300

    feat About chapter adapted for All

--- Routes/About.js
+++ Routes/About.js
@@ -16,20 +16,16 @@ export const About = {
         </p>
         <div class="About-figures" id="medievalPics">
            <figure>
-                <img src="./pics/Morignoni_Desenciano.jpg" id='Morignoni' class="about-pic" alt="portrait of young tailor Morignoni Desenciano">
+                <img src="./pics/Morignoni_Desenciano.jpg" class="about-pic" alt="portrait of young tailor Morignoni Desenciano">
                 <figcaption> Портрет Маринони Дезенцано, Лондон, 1525г.</figcaption>
-           </figure>
-           <figure>
-                <img src="./pics/Henry_8.jpeg" id='Henry' class="about-pic" alt="portrait of King Henry VIII">
-                <figcaption> Портрет Короля Генриха VIII, Лондон, 1528г.</figcaption>
            </figure>
           <p class="About-description about_over_1201">
             Почти вековая история модного дома Clothery начинается в довоенном Лондоне.<br/>
             Его основатель Чезаре Феллини приходился потомком самому Маринони Дезенцано - портному при дворе короля Генриха VIII Тюдора.<br/><br/>
-            В 1930 году юный Чезаре, тогда ещё помощник закройщика, открывает небольшую мастерскую по ремонту одежды. 
+            В 1930 году юный Чезаре, тогда ещё помощник закройщика, открывает небольшую мастерскую по ремонту одежды.
             Поначалу дела шли неважно. Волею случая, его клиенткой становится Виктория Хьюз, жена офицера королевской гвардии.
             Вскоре мастерская Чезаре начинает сотрудничать с армией Её Величества.
-            Сначала шли заказы только на ремонт офицерских мундиров. 
+            Сначала шли заказы только на ремонт офицерских мундиров.
             Но в 1932 году открылась военная Академия Британской Индийской армии, и слушателям потребовалась униформа нового образца.
             Тогда Чезаре получает свой первый заказ на пошив.
             Успешно выполнив его, в 1933 году он даёт мастерской название <b><i>Clothery</i></b> <i>(по аналогии с привычными англичанам
@@ -38,6 +34,11 @@ export const About = {
             имя Clothery приобретает широкую известность среди мирного населения. Вторым витком развития стало
             появление коллекции от Чезаре на Неделе Моды в Лондоне в 1940 году.<br/><br/>
           </p>
+           <figure>
+                <img src="./pics/Henry_8.jpeg" class="about-pic" alt="portrait of King Henry VIII">
+                <figcaption> Портрет Короля Генриха VIII, Лондон, 1528г.</figcaption>
+           </figure>
+          
           <p class="About-description about_upto480">В 1930 году юный Чезаре, тогда ещё помощник закройщика, открывает небольшую мастерскую по ремонту одежды.
             Поначалу дела шли неважно. Волею случая, его клиенткой становится Виктория Хьюз, жена офицера королевской
             гвардии.
@@ -50,7 +51,7 @@ export const About = {
             Сначала шли заказы только на ремонт офицерских мундиров.</p>
         </div>
           
-        <div class="About-figures" id="20th_pics">
+        <div class="About-figures" id="twentys_pics">
           <p class="About-description about_horizTabl">Но в 1932 году открылась военная Академия Британской Индийской армии, 
             и слушателям потребовалась униформа
             нового образца.Тогда Чезаре получает свой первый заказ на пошив.
@@ -60,15 +61,9 @@ export const About = {
             Сотрудничество с войсками стало основной движущей силой и вектором развития. Лишь к 1938 году
             имя Clothery приобретает широкую известность среди мирного населения.
           </p>
-          <p class="About-description about_over_1201">
-            Начало Второй Мировой войны могло бы вновь вернуть бренд к "военным истокам", но
-            Чезаре решает не оставлять гражданскую моду, ибо войны рано или поздно заканчиваются.
-            Тем не менее, заработав себе имя именно как армейский поставщик, он создаёт для потребностей армии
-            дочерний филиал, который в дальнейшем выделится в отдельный бренд,  сменит хозяина и станет известен
-            как <a href="https://aeronauticamilitare.eu/">Aeronautica Militare</a>.
-          </p>
-          <figure>
-                <img src="./pics/Chesare_Fellini_1930s.jpg" id='Chesare' class="about-pic" alt="portrait of tailor Chesare Fellini">
+          
+          <figure class="20th_figure">
+                <img src="./pics/Chesare_Fellini_1930s.jpg" class="about-pic" alt="portrait of tailor Chesare Fellini">
                 <figcaption> Чезаре Феллини в мастерской, 1930-е гг, Лондон</figcaption>
             </figure>
           <p class="About-description about_upto480">Но в 1932 году открылась военная Академия Британской Индийской армии, и слушателям потребовалась униформа
@@ -79,8 +74,15 @@ export const About = {
               "grocery"(бакалея) или "bakery"(булочная))</i> и открывает при ней магазин готовой одежды.<br/><br/>
             Сотрудничество с войсками стало основной движущей силой и вектором развития. Лишь к 1938 году
             имя Clothery приобретает широкую известность среди мирного населения.</p>
-          <figure>
-                <img src="./pics/British_Indian_Army.png" id='Indians' class="about-pic" alt="three British Indian Army officers">
+          <p class="About-description about_over_1201">
+            Начало Второй Мировой войны могло бы вновь вернуть бренд к "военным истокам", но
+            Чезаре решает не оставлять гражданскую моду, ибо войны рано или поздно заканчиваются.
+            Тем не менее, заработав себе имя именно как армейский поставщик, он создаёт для потребностей армии
+            дочерний филиал, который в дальнейшем выделится в отдельный бренд,  сменит хозяина и станет известен
+            как <a href="https://aeronauticamilitare.eu/">Aeronautica Militare</a>.
+          </p>
+          <figure class="20th_figure">
+                <img src="./pics/British_Indian_Army.png" class="about-pic" alt="three British Indian Army officers">
                 <figcaption>Выпускники Академии, 1930-е гг, Лондон</figcaption>
           </figure>
           <p class="About-description about_upto480">Вторым витком развития стало
